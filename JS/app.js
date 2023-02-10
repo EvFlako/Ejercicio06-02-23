6e3784140af9d7d61e3bacd32482639f5eb36e36
user.email <fabriziobagnasco2001@gmail.com>
2023-02-10 18:51:29 -0300

    Boton eliminar JS

--- JS/app.js
+++ JS/app.js
@@ -3,9 +3,14 @@ const lista = document.getElementById('listaTareas');
 
 
 function cargarTarea(){
+    const cuerpoTarea = document.createElement ("div");
  const item = document.createElement('li');
  item.textContent = tarea.value;
- lista.appendChild(item);
+ item.style = "list-style-type:none";
  tarea.value = '';
+ const botonEliminar = document.createElement("button");
+ cuerpoTarea.append(item,botonEliminar);
+ lista.appendChild(cuerpoTarea);
+ cuerpoTarea.style = "display:flex;justify-content:space-between"
 }
 
