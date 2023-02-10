1f1a76ac4c3438bd23cf8a5e447ecc80fe0b834f
gabriel-amaya <evflako@gmail.com>
2023-02-10 20:15:38 -0300

    tarea completa

--- JS/app.js
+++ JS/app.js
@@ -2,15 +2,39 @@ const tarea = document.getElementById('tarea');
 const lista = document.getElementById('listaTareas');
 
 
+const tareasVarias = [];
+
+
 function cargarTarea(){
-    const cuerpoTarea = document.createElement ("div");
- const item = document.createElement('li');
- item.textContent = tarea.value;
- item.style = "list-style-type:none";
- tarea.value = '';
- const botonEliminar = document.createElement("button");
- cuerpoTarea.append(item,botonEliminar);
- lista.appendChild(cuerpoTarea);
- cuerpoTarea.style = "display:flex;justify-content:space-between"
+    lista.textContent = "";
+    tareasVarias.push(tarea.value);
+    crear();
 }
 
+
+function crear() {
+    tareasVarias.map(
+    function agregar(t) {
+        const cuerpoTarea = document.createElement ("div");
+        const item = document.createElement('li');
+        item.textContent = t;
+        item.style = "list-style-type:none";
+        tarea.value = '';
+        const botonEliminar = document.createElement("button");
+        cuerpoTarea.append(item,botonEliminar);
+        lista.appendChild(cuerpoTarea);
+        cuerpoTarea.style = "display:flex; justify-content:space-between; align-items: center;";
+        cuerpoTarea.classList = "form-control";
+        botonEliminar.classList = "btn btn-danger";
+        botonEliminar.textContent = 'Eliminar';
+        botonEliminar.onclick = eliminarTarea
+    }
+ );
+}
+
+function eliminarTarea(indice) {
+    tareasVarias.splice(indice, 1);
+    console.log(tareasVarias);
+    lista.innerHTML = "";
+    crear();
+};
