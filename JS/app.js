d5ee9f2bfbba61d5945859b9c2e6ff4612187824
David Smit'h <115672052+FDavidSmith@users.noreply.github.com>
2023-02-10 21:38:23 -0300

    se finalizan estilos

--- JS/app.js
+++ JS/app.js
@@ -17,13 +17,14 @@ function crear() {
         const cuerpoTarea = document.createElement ("div");
         const item = document.createElement('li');
         item.textContent = t;
-        item.style = "list-style-type:none";
+        item.style = "list-style-type:none; font-family: Miltonian Tattoo; margin-left: 10px; font-size: 20px";
         tarea.value = '';
         const botonEliminar = document.createElement("button");
         cuerpoTarea.append(item,botonEliminar);
         lista.appendChild(cuerpoTarea);
         cuerpoTarea.style = "display:flex; justify-content:space-between; align-items: center;";
-        cuerpoTarea.classList = "form-control";
+        lista.classList = "row container-fluid"
+        cuerpoTarea.classList = "form-control col col-8 col-sm-8";
         botonEliminar.classList = "btn btn-danger";
         botonEliminar.textContent = 'Eliminar';
         botonEliminar.onclick = ()=> eliminarTarea(tareasVarias.indexOf(t));
